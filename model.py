8ff4a37835c48c6840a611858980bb3fc329a0a3
luzh <597203933@qq.com>
2017-02-17 22:38:14 +0800

    add annotation

--- model.py
+++ model.py
@@ -29,11 +29,11 @@ class Model(object):
     def __init__(self, config):
         self.config = config
         self.load_data()
-        self.vocab_size = len(self.index2token)
+        self.vocab_size = len(self.index2token)  #8834
 
         # placeholders
-        self._sent_placeholder = tf.placeholder(tf.int32, shape=[self.config.batch_size, None], name='sent_ph')
-        self._img_placeholder = tf.placeholder(tf.float32, shape=[self.config.batch_size, self.config.img_dim], name='img_ph')
+        self._sent_placeholder = tf.placeholder(tf.int32, shape=[self.config.batch_size, None], name='sent_ph')  #None代表n_steps
+        self._img_placeholder = tf.placeholder(tf.float32, shape=[self.config.batch_size, self.config.img_dim], name='img_ph') # lstm unfold的第一维就是图片
         self._targets_placeholder = tf.placeholder(tf.int32, shape=[self.config.batch_size, None], name='targets')
         self._dropout_placeholder = tf.placeholder(tf.float32, name='dropout_placeholder')
 
@@ -42,14 +42,15 @@ class Model(object):
         with tf.variable_scope('CNN'):
             W_i = tf.get_variable('W_i', shape=[self.config.img_dim, self.config.embed_dim])
             b_i = tf.get_variable('b_i', shape=[self.config.batch_size, self.config.embed_dim])
-            img_input = tf.expand_dims(tf.nn.sigmoid(tf.matmul(self._img_placeholder, W_i) + b_i), 1)
+            img_input = tf.expand_dims(tf.nn.sigmoid(tf.matmul(self._img_placeholder, W_i) + b_i), 1)  # 256*1*512 256表示batch_size 512表示输入向量维度
             print 'Img:', img_input.get_shape()
         with tf.variable_scope('sent_input'):
+            # self.vocab_size 表示单词大小 self.config.embed_dim 表示一个单词转换为特征向量的维度   _sent_placeholder将单词转为向量维度
             word_embeddings = tf.get_variable('word_embeddings', shape=[self.vocab_size, self.config.embed_dim])
             sent_inputs = tf.nn.embedding_lookup(word_embeddings, self._sent_placeholder)
-            print 'Sent:', sent_inputs.get_shape()
+            print 'Sent:', sent_inputs.get_shape()   # 256×?*512 其中256是batch_size ?表示n_steps-1  512表示输入向量维数
         with tf.variable_scope('all_input'):
-            all_inputs = tf.concat(1, [img_input, sent_inputs])    
+            all_inputs = tf.concat(1, [img_input, sent_inputs])    # 256*?*512  img_input 和sent_inputs连在一起了
             print 'Combined:', all_inputs.get_shape()
 
 
@@ -58,8 +59,10 @@ class Model(object):
         lstm_dropout = tf.nn.rnn_cell.DropoutWrapper(lstm, input_keep_prob=self._dropout_placeholder, output_keep_prob=self._dropout_placeholder)
         stacked_lstm = tf.nn.rnn_cell.MultiRNNCell([lstm_dropout] * self.config.layers)
         initial_state = stacked_lstm.zero_state(self.config.batch_size, tf.float32)
+        # outputs 256*?*512  其中256是batch_size ?是n_steps 512是维数，因为n_steps大小不固定，所以是dynamic_rnn
         outputs, final_state = tf.nn.dynamic_rnn(stacked_lstm, all_inputs, initial_state=initial_state, scope='LSTM')
         output = tf.reshape(outputs, [-1, self.config.hidden_dim]) # for matrix multiplication
+        # 将每个样例的n_step放在了一起，变成了(256*?) * 512  其中每n_steps个512 表示一个样例所有的ouputs
         self._final_state = final_state
         print 'Outputs (raw):', outputs.get_shape()
         print 'Final state:', final_state.get_shape()
@@ -67,21 +70,23 @@ class Model(object):
 
 
         # Softmax layer
-        with tf.variable_scope('softmax'):
+        with tf.variable_scope('softmax'):   #每个unfolding的w和b共享权重
             softmax_w = tf.get_variable('softmax_w', shape=[self.config.hidden_dim, self.vocab_size])
             softmax_b = tf.get_variable('softmax_b', shape=[self.vocab_size])
-            logits = tf.matmul(output, softmax_w) + softmax_b
+            logits = tf.matmul(output, softmax_w) + softmax_b   #结果为(256*?)*vocab_size  ?表示n_steps个数，前n_steps行表示一个样例所对应的全部输出， 共享softmax_w和softmx_b
         print 'Logits:', logits.get_shape()
 
 
         # Predictions
         self.logits = logits
         self._predictions = predictions = tf.argmax(logits,1)
+        # 返回每一行最大元素所在的下标，这样得到的维数就是 (256*?)*1
+        # 内容为最大元素所对应的下标， 故前n_steps行表示的是第一个样例所对应的每个概率单词的id
         print 'Predictions:', predictions.get_shape()
 
 
         # Minimize Loss
-        targets_reshaped = tf.reshape(self._targets_placeholder,[-1])
+        targets_reshaped = tf.reshape(self._targets_placeholder,[-1])  # 将target转为(256*?)*1
         print 'Targets (raw):', self._targets_placeholder.get_shape()
         print 'Targets (reshaped):', targets_reshaped.get_shape()
         with tf.variable_scope('loss'):
@@ -98,17 +103,19 @@ class Model(object):
     def load_data(self, type='train'):
         if type == 'train':
             with open('data_files/index2token.pkl','r') as f:
-                self.index2token = pickle.load(f)
+                self.index2token = pickle.load(f) #id对应的字典 如同{0:u'raining', 1:u'writing', 2:'yellow', ...8841:'awake', '8842:'<EOS>'}
             with open('data_files/preprocessed_train_captions.pkl','r') as f:
+                #train_captions: {sentence长度:train_caption_id}
                 self.train_captions, self.train_caption_id2sentence, self.train_caption_id2image_id = pickle.load(f)
             with open('data_files/train_image_id2feature.pkl','r') as f:
                 self.train_image_id2feature = pickle.load(f)
 
 
     def run_epoch(self, session, train_op):
+        # 一次epoch迭代所有样本次数，total_step为迭代完所有样本需要的mini_batch次数
         total_steps = sum(1 for x in train_data_iterator(self.train_captions, self.train_caption_id2sentence, self.train_caption_id2image_id, self.train_image_id2feature, self.config))
         total_loss = []
-        if not train_op:
+        if not train_op:   # 不是训练过程
             train_op = tf.no_op()
         start = time.time()
 
@@ -135,7 +142,8 @@ class Model(object):
 
         sent_pred = np.ones([self.config.batch_size, 1])*3591 # <SOS>
         while sent_pred[0,-1] != 3339 and (sent_pred.shape[1] - 1) < 50:
-            feed = {self._sent_placeholder: sent_pred,
+           # sent_pred[0,-1]表示第0行最后一个词不是id=33339对应的词语(表示结束)，那么生成的caption小于50才执行
+            feed = {self._sent_placeholder: sent_pred,    # lstm unfold 每循环一次增长一次
                     self._img_placeholder: img_template,
                     self._targets_placeholder: np.ones([self.config.batch_size,1]), # dummy variable
                     self._dropout_placeholder: dp}
@@ -194,17 +202,17 @@ def evaluateModel(model_json):
     return results
 
 def main():
-    config = Config()
+    config = Config()   # lstm配置信息
     with tf.variable_scope('CNNLSTM') as scope:
-        model = Model(config)
+        model = Model(config)    #创建model
     loss_history = []
     all_results_json = {}
-    init = tf.initialize_all_variables()
-    saver = tf.train.Saver()
+    init = tf.initialize_all_variables()    #初始化所有的变量
+    saver = tf.train.Saver()      # 会话保存信息
 
     with tf.Session() as session:
         session.run(init)
-        for epoch in range(config.max_epochs):
+        for epoch in range(config.max_epochs):    #执行epoch迭代
             ## train model
             print 'Epoch %d' % (epoch+1)
             total_loss =  model.run_epoch(session, model.train_op)
--- utils.py
+++ utils.py
@@ -13,11 +13,12 @@ def sample(a, temperature=1.0):
 
 def get_batches(train_captions, batch_size):
     train_batches = []
+    # train_caption 为{id1:set1, id2:set2,...}  其中id为set中id(对应sentence)的数量，长度为id的sentence的id的结合为set
     for sent_length, caption_set in train_captions.items():
         caption_set = list(caption_set)
         random.shuffle(caption_set)
         num_captions = len(caption_set)
-        num_batches = num_captions // batch_size
+        num_batches = num_captions // batch_size   # 必须选择caption中id对应的set集合在256以上才加入，这样才能保证minbatch=256
         for i in range(num_batches+1):
             end_idx = min((i+1)*batch_size, num_captions)
             new_batch = caption_set[(i*batch_size):end_idx]
@@ -27,10 +28,10 @@ def get_batches(train_captions, batch_size):
     return train_batches
 
 def formatPlaceholder(batch_item, batch_size, img_dim, train_caption_id2sentence, train_caption_id2image_id, train_image_id2feature):
-    (caption_ids, sent_length) = batch_item
-    num_captions = len(caption_ids)
+    (caption_ids, sent_length) = batch_item   # caption_ids为sentence的对应的id，是一个列表，他们拥有相同的长度sent_length
+    num_captions = len(caption_ids)   # 等于batch_size的大小，每个batch_size中短语的长度是一致的，等于lstm中n_step-1
     sentences = np.array([train_caption_id2sentence[k] for k in caption_ids])
-    images = np.array([train_image_id2feature[train_caption_id2image_id[k]] for k in caption_ids])
+    images = np.array([train_image_id2feature[train_caption_id2image_id[k]] for k in caption_ids])  # 变成了256×img_dim
     targets = sentences[:,1:]
     
     sentences_template = np.zeros([batch_size, sent_length])
@@ -47,7 +48,7 @@ def formatPlaceholder(batch_item, batch_size, img_dim, train_caption_id2sentence
 def train_data_iterator(train_captions, train_caption_id2sentence, train_caption_id2image_id, train_image_id2feature, config):
     batch_size = config.batch_size
     img_dim = config.img_dim
-    
+    # 返回的是[(batch_size1,对应短语长度1)，(batch_size2, 对应短语长度2)，..] batch的长度为256，是一个列表，每个短语对应一个id，通过self.caption_id2sentence能找到对应的sentence是什么
     train_batches = get_batches(train_captions, batch_size)
     for batch_item in train_batches:
         sentences, images, targets = formatPlaceholder(batch_item, batch_size, img_dim, train_caption_id2sentence, train_caption_id2image_id, train_image_id2feature,)
